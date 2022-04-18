ed7270288a0cb09301ac72a77d55b9b7a1878276
jaymesC <clementjaymes@gmail.com>
2022-04-18 13:11:02 -0700

    added new pages and screens

---
+++ lib/pages/navpages/contactlist_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  const ContactList({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
