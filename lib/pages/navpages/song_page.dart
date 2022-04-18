b4e6f0f526edb63efbdabbc5eb9c74adc6ef6cf8
jaymesC <clementjaymes@gmail.com>
2022-04-18 13:25:48 -0700

    updated pages

---
+++ lib/pages/navpages/song_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MusicPage extends StatelessWidget {
+  const MusicPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Music page')
+      ),
+    );
+  }
+}
