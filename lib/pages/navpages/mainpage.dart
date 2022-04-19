f0d8984d050aa03caf890f0b01eef9f17020beb3
jaymesC <clementjaymes@gmail.com>
2022-04-19 15:21:11 -0700

    updated homepage

--- lib/pages/navpages/mainpage.dart
+++ lib/pages/navpages/mainpage.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:safeplace_app/pages/home_page.dart';
+import 'package:safeplace_app/pages/navpages/contactlist_page.dart';
+import 'package:safeplace_app/pages/navpages/message_page.dart';
+import 'package:safeplace_app/pages/navpages/mypage.dart';
+import 'package:safeplace_app/pages/navpages/song_page.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    ContactList(),
+    MessagePage(),
+    MusicPage(),
+    MyPage()
+    ];
+
+    int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main Page')
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: onTap,
+        currentIndex:currentIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.deepPurple.withOpacity(0.8),
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled)),
+          BottomNavigationBarItem(icon: Icon(Icons.people_outline_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled)),
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled)),
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled)),
+        ],
+
+
+
       ),
-      bottomNavigationBar: BottomNavigationBar(),
     );
   }
 }
